fab02505c23883b33a099ee3a5e22b1d766a47f0
Steve Lynch <slynch@qwinixtech.com>
2016-12-22 08:51:21 -0700

    add career listing on contact page

--- contact.php
+++ contact.php
@@ -180,6 +180,10 @@ margin-right: 10px;
         	<h2>Contact WonderOps</h2>
              <div class="why-cho_inner_subtext" >
              
+<b>Currently Seeking:</b> full stack Software Developer experienced with MEAN stack in Boulder, CO. 
+
+<br><br>
+
 Please use this form to connect with us! Let us know if you are an employer or IT professional. Our default action is to include you in our mailing list appropriate to your interest. If you would prefer not to be on our mailing list, let us know in the comments. We look forward to working with you.
 <?php if($success){?>
 <div class="box success"><strong>SUCCESS!</strong> Your message was successfully sent.</div>
